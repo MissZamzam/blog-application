0478407d136c8874494ad32111b8025d1e9de24a
Zamzam <Zamzamhassan44@gmail.com>
2022-07-28 14:53:54 +0300

    imported and nested Navbar.js inside  App.js,added Home.js,About.js,Contact.js and Create.js inside the Navbar component as  anchor tags

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import Navbar from "./Navbar";
 
 function App() {
+
   return (
     <div className="App">
+      <Navbar />
+      <div className="container"></div>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,17 @@
 function Navbar () {
-    return 
+    return (
+
+        <div className="navbar">
+            <h1>The WEEKLY Blog</h1>
+        <div className="container">
+          <a href="/">Home</a>
+          <a href="/create">New Blog</a>
+          <a href="about">About</a>
+          <a href="contact">Contact</a>
+        </div>
+        </div>
+
+    )
 }
  
 export default Navbar;
