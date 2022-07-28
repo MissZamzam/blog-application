a0dc33f8ad836788056be7082b2e2f4b4307c794
Zamzam <Zamzamhassan44@gmail.com>
2022-07-29 00:18:34 +0300

    made a DELETE request inside the Create component

--- src/ArticleRoutes.js
+++ src/ArticleRoutes.js
@@ -4,6 +4,16 @@ import useFetch from "./useFetch";
 function ArticalRoutes () {
     const {id} = useParams()
     const {data : article} = useFetch('http://localhost:3000/articles' + id)
+
+    function handleDelete(){
+        fetch('http://localhost:3000/articles/' + article.id,{
+            method: 'DELETE',
+        }).then((response) => response.json())
+        .then(() => console.log('deleted'))
+            
+        }
+
+    
     return (
         <div className="article-details">
             {article && (
@@ -11,6 +21,7 @@ function ArticalRoutes () {
                     <h2>{article.title}</h2>
                     <p>By{article.author}</p>
                     <div>{article.body}</div>
+                    <button onClick={handleDelete}>Delete</button>
                 </piece>
             )}
         </div>
