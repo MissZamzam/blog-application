c1a1d6749999abd11dedca8d4a2bbe21f8bb358a
Zamzam <Zamzamhassan44@gmail.com>
2022-07-28 15:19:00 +0300

    used useState hook to create the state

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,14 @@
+import { useState } from "react";
 
 function Home () {
+
+    const [articles, setArticles] = useState([
+        {title: 'My vacation to MiddleEast', body: 'abhk...', author: 'Mike John', id:1},
+        {title: 'Politics in Fashion Industry', body: 'abhk...', author: 'Miriam Williams', id:2},
+        {title: 'My journey as a developer', body: 'abhk...', author: 'Joseph', id:3},
+        {title: 'Favorite cookbook', body: 'abhk...', author: 'Jerry', id:4}
+
+    ])
     return (
         <div className="home">
             
