505daeaca8923f5659a9afddc5e9cde41070e26b
Zamzam <Zamzamhassan44@gmail.com>
2022-07-29 23:52:35 +0300

    added more links to resources component and added styles too

--- src/More.js
+++ src/More.js
@@ -1,10 +1,22 @@
 function Resources () {
     return (  
         <div className="resource">
-            <h2 className="heading">Resource Categories</h2>
+            <h2 className="heading">Categories</h2>
         <div className="policy">
             <h2>Policitcs</h2>
+            <a href="https://www.project-syndicate.org/commentary/sri-lanka-rajapaksa-ouster-moment-of-ethnic-solidarity-by-priyanka-krishnamoorthy-1-2022-07?barrier=accesspaylog">Sri Lanka’s Next Test</a>
+            <br></br>
             <a href="https://www.project-syndicate.org/section/politics-world-affairs?utm_term=%2Bworld%20%2Bpolitics&utm_campaign=&utm_source=adwords&utm_medium=ppc&hsa_acc=1220154768&hsa_cam=12372417053&hsa_grp=115837872937&hsa_ad=499608749641&hsa_src=g&hsa_tgt=kwd-297496700368&hsa_kw=%2Bworld%20%2Bpolitics&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gclid=EAIaIQobChMI47eYrdOe-QIVx-F3Ch3pzQvYEAAYASAAEgIxlvD_BwE">Politics & World Affairs</a>
+            <br></br>
+            <a href="https://nation.africa/kenya/news/facebook-operations-risk-being-suspended-over-hate-speech--3896348">Facebook operations risk being suspended over hate speech</a>
+            </div>
+            <div className="sports">
+                <h2>Sports</h2>
+                <a href="https://nation.africa/kenya/sports/rugby/kenya-7s-thump-jamaica-storm-quarters-commonwealth-games-3896812">Kenya 7s thump Jamaica to storm quarters in Birmingham</a>
+                <br></br>
+                <a href="https://nation.africa/kenya/sports/athletics">Doping? Kenya down to just one athlete in women’s marathon</a>
+                <br></br>
+                <a href="https://www.dailymail.co.uk/sport/football/article-11062605/Sir-Alex-Ferguson-Manchester-United-order-help-Erik-ten-Hags-rebuild.html">Sir Alex Ferguson is Back at Manchester United to help the club</a>
             </div>
          <div className="fashion">
             <h2>Fashion</h2>
