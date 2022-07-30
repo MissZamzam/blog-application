5748a65f4b1bb6baea51209f5d678fe4a9552ce1
Zamzam <Zamzamhassan44@gmail.com>
2022-07-30 21:42:29 +0300

    added style

--- src/Contact.js
+++ src/Contact.js
@@ -2,34 +2,16 @@ const Contact = () => {
     return (
         <div className="contacts">
            <h2 className="two">Contact</h2>
-           <div className="info">
-            <div className="address">
-                <div className="sub-div">
-                    <div className="text">
                         <h4 className="four">Address</h4>
                         <p>First Floor, Forest RD,<br />First Lane100-097.</p>
-                    </div>
-                </div>
-            </div>
-            <div className="phone">
-                <div className="sub-div">
-                    <div className="text">
                         <h4 className="four">Phone</h4>
                         <p>07123445678</p>
                         <p>0205267728</p>
-                    </div>
-                </div>
-            </div>
-            <div className="email">
-                <div className="sub-div">
-                    <div className="text">
                         <h4 className="four">Email</h4>
                         <p>abz@theweeklyblog.com</p>
+                        <p>abz@theweeklyblog.co.ke</p>
+
                     </div>
-                </div>
-            </div>
-           </div>
-        </div>
     )
    
 }
